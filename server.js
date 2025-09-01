c17de4c9bee6221ddd9683b90a86e8a0befb31f7
frnmotorserp <rajifamondal24@gmail.com>
2025-09-01 17:35:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5080;
 // Enable All CORS Requests
 app.use(cors({
   //origin: 'http://localhost:4000',  
-  origin: "https://erpsfrontend.onrender.com",  
+  origin: "https://frnmotors.onrender.com",  
   credentials: true,               
 }));
 app.use(express.json());
