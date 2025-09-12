a73f8d34cc46be207d3be4dd5b8b1dca6538c2df
frnmotorserp <rajifamondal24@gmail.com>
2025-09-12 16:55:09 +0530

    Update invoicePaymentManagementRoutes.js

--- routes/invoicePaymentManagementRoutes.js
+++ routes/invoicePaymentManagementRoutes.js
@@ -2,7 +2,15 @@ import express from 'express';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
 import { getPaymentsGroupedByInvoiceController, getInvoiceWithItemsController,
-      getInvoicesByFiltersController, saveOrUpdateInvoiceController, getPaymentsByInvoiceIdController, syncPaymentsForInvoiceController } from '../controllers/invoicePaymentController.js';
+      getInvoicesByFiltersController, 
+      saveOrUpdateInvoiceController, getPaymentsByInvoiceIdController, 
+      syncPaymentsForInvoiceController,
+      createCashEntry,
+      editCashEntry,
+      removeCashEntry,
+      listCashEntries,
+      fetchCashBalance,
+} from '../controllers/invoicePaymentController.js';
 
 
 const router = express.Router();
@@ -13,7 +21,11 @@ router.post('/managePayments', verifyToken,  syncPaymentsForInvoiceController);
 router.post('/getAllPayments', verifyToken,  getPaymentsByInvoiceIdController);
 router.post('/getPaymentsGroupedByInvoice', verifyToken,  getPaymentsGroupedByInvoiceController);
 router.post('/getInvoiceWithItems', verifyToken,  getInvoiceWithItemsController);
-
+router.post("/createCashEntry", verifyToken, createCashEntry);
+router.post("/editCashEntry", verifyToken, editCashEntry);
+router.post("/removeCashEntry", verifyToken, removeCashEntry);
+router.post("/listCashEntries", verifyToken, listCashEntries);        // ?startDate=2025-09-01&endDate=2025-09-03
+router.post("/fetchCashBalance", verifyToken, fetchCashBalance);
 
 
 export default router;
